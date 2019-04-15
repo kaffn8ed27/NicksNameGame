31630728b8f2f904ffd0468d2f7bf5b3ab2701bf
Nick Reeve <nick.reeve@willowtreeapps.com>
2019-04-15 10:43:00 -0400

    changed game screen to RecyclerView, added LayoutManager and Adapter

--- app/src/main/java/android/example/nicksnamegame/game/GameActivity.java
+++ app/src/main/java/android/example/nicksnamegame/game/GameActivity.java
@@ -1,6 +1,8 @@
 package android.example.nicksnamegame.game;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.example.nicksnamegame.R;
 import android.os.Bundle;
@@ -10,51 +12,26 @@ import android.view.View;
 public class GameActivity extends AppCompatActivity
         implements View.OnClickListener {
 
+    private static final int NUM_COWORKERS = 500;
+
+    private RecyclerView photosList;
+    private PhotoAdapter photoAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        photosList = (RecyclerView) findViewById(R.id.rv_photos);
+
+        LinearLayoutManager photoManager = new LinearLayoutManager(this);
+        photosList.setLayoutManager(photoManager);
+        photosList.setHasFixedSize(true);
     }
 
     @Override
     public void onClick(View v) {
 
-        int id = v.getId();
-
-        switch (id) {
-            case R.id.photo_1:
-                Log.d("Photo clicked: ", getString(R.string.photo_label_1));
-                findViewById(R.id.photo_label_1).setVisibility(View.VISIBLE);
-                v.setBackground(getDrawable(R.drawable.chose_poorly));
-                break;
-
-            case R.id.photo_2:
-                Log.d("Photo clicked: ", getString(R.string.photo_label_1));
-                findViewById(R.id.photo_label_2).setVisibility(View.VISIBLE);
-                v.setBackground(getDrawable(R.drawable.chose_poorly));
-                break;
-
-            case R.id.photo_3:
-                Log.d("Photo clicked: ", getString(R.string.photo_label_1));
-                findViewById(R.id.photo_label_3).setVisibility(View.VISIBLE);
-                v.setBackground(getDrawable(R.drawable.chose_poorly));
-                break;
-
-            case R.id.photo_4:
-                Log.d("Photo clicked: ", getString(R.string.photo_label_1));
-                findViewById(R.id.photo_label_4).setVisibility(View.VISIBLE);
-                v.setBackground(getDrawable(R.drawable.chose_wisely));
-                break;
-
-            case R.id.photo_5:
-                Log.d("Photo clicked: ", getString(R.string.photo_label_1));
-                findViewById(R.id.photo_label_5).setVisibility(View.VISIBLE);
-                v.setBackground(getDrawable(R.drawable.chose_poorly));
-                break;
-
-            default:
-                break;
-        }
     }
 }
+
