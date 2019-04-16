15495166c3b388c6915226f754c3f44f09e218e1
Nick Reeve <nick.reeve@willowtreeapps.com>
2019-04-16 14:45:46 -0400

    Changed launch text to button; tweaked look of game screen

--- app/src/main/java/android/example/nicksnamegame/launcher/MainActivity.java
+++ app/src/main/java/android/example/nicksnamegame/launcher/MainActivity.java
@@ -7,18 +7,19 @@ import android.example.nicksnamegame.R;
 import android.example.nicksnamegame.game.GameActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView gameLauncherTextView;
+    private Button defaultGameButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        gameLauncherTextView = findViewById(R.id.default_game_text_view);
-        gameLauncherTextView.setOnClickListener(new View.OnClickListener() {
+        defaultGameButton = findViewById(R.id.default_game_button);
+        defaultGameButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent startGameIntent = new Intent(MainActivity.this, GameActivity.class);
