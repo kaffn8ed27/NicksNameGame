3b18005c98045c5200984c476091706b6cfd5c3b
Nick Reeve <nick.reeve@willowtreeapps.com>
2019-04-23 13:29:14 -0400

    set up API response handling

--- app/src/main/java/android/example/nicksnamegame/game/GameActivity.java
+++ app/src/main/java/android/example/nicksnamegame/game/GameActivity.java
@@ -1,21 +1,21 @@
 package android.example.nicksnamegame.game;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.example.nicksnamegame.R;
 import android.example.nicksnamegame.data.model.PersonConverter;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import java.util.List;
 
 public class GameActivity extends AppCompatActivity {
 
-    private static final boolean SHOULD_USE_LIVE_DATA = false; // cannot be set to true until Picasso is implemented
-
-    private List<Person> personList;
+    private static final String TAG = GameActivity.class.getSimpleName();
 
     // TODO - add "next group" functionality
     /* TODO - tracking: if answered right on the first try, remove coworker from the pool.
@@ -41,32 +41,39 @@ public class GameActivity extends AppCompatActivity {
         people.setHasFixedSize(true);
 
 
-        if (SHOULD_USE_LIVE_DATA) {
-            personList = new PersonConverter().retrievePersonList();
-            peopleShuffler = new PeopleShuffler(personList);
-            peopleShuffler.chooseCoworkers();
-            // TODO: `use Picasso to set the image views
-        } else {
-            peopleShuffler = new PeopleShuffler();
-            // create a shuffled list of people: Beatles demo version
-            shuffledList = peopleShuffler.createShuffledList(this);
-        }
+            new PersonConverter().retrievePersonList(new PersonConverter.PersonListHandler() {
+                @Override
+                public void onReceivePersonList(List<Person> personList) {
+                    if (personList != null) {
+                        peopleShuffler = new PeopleShuffler(personList);
+                        shuffledList = peopleShuffler.chooseCoworkers();
+                    } else {
+                        Log.d(TAG, "List of co-workers not found");
+                    }
 
-        // pass the list of people to the adapter
-        photoAdapter = new PhotoAdapter(shuffledList);
-        people.setAdapter(photoAdapter);
+                    if(shuffledList != null) {
+                        photoAdapter = new PhotoAdapter(shuffledList, GameActivity.this);
+                        people.setAdapter(photoAdapter);
+                    }
 
-        // set the text of the game prompt
-        game_prompt_text_view = findViewById(R.id.game_prompt);
-        game_prompt_text_view.setText(setName());
+                    // set the text of the game prompt
+                    game_prompt_text_view = findViewById(R.id.game_prompt);
+                    game_prompt_text_view.setText(setName());
+                }
+            });
     }
 
     private String setName() {
         // extract the correct name from the ShuffledList object and set the prompt text
-        int index = shuffledList.getIndex();
-        List<Person> peopleToChooseFrom = shuffledList.getPeople();
-        String name = peopleToChooseFrom.get(index).getName();
-        String namePrompt = "Who is " + name + "?";
-        return namePrompt;
+        String namePrompt;
+        if(shuffledList != null) {
+            int index = shuffledList.getIndex();
+            List<Person> peopleToChooseFrom = shuffledList.getPeople();
+            String name = peopleToChooseFrom.get(index).getName();
+            namePrompt = "Who is " + name + "?";
+        } else {
+            namePrompt = getString(R.string.generic_error);
+        }
+            return namePrompt;
     }
 }
--- app/src/main/java/android/example/nicksnamegame/game/PeopleShuffler.java
+++ app/src/main/java/android/example/nicksnamegame/game/PeopleShuffler.java
@@ -15,7 +15,7 @@ public class PeopleShuffler {
 
 
     private static final String TAG = PeopleShuffler.class.getSimpleName();
-    private static final int NUM_COWORKERS_TO_SHOW = 6;
+    private static final int NUM_COWORKERS_TO_SHOW = 12;
     private List<Person> personList = new ArrayList<>();
     int correctAnswerIndex;
 
@@ -25,29 +25,7 @@ public class PeopleShuffler {
         this.personList = personList;
     }
 
-    public ShuffledList createShuffledList (Context context) {
-
-
-        Person john = new Person(context.getString(R.string.John), R.drawable.john);
-        Person paul = new Person(context.getString(R.string.Paul), R.drawable.paul);
-        Person george = new Person(context.getString(R.string.George), R.drawable.george);
-        Person ringo = new Person(context.getString(R.string.Ringo), R.drawable.ringo);
-        Person yoko = new Person(context.getString(R.string.Yoko), R.drawable.yoko);
-
-        personList.add(john);
-        personList.add(paul);
-        personList.add(george);
-        personList.add(ringo);
-        personList.add(yoko);
-
-        Collections.shuffle(personList);
-
-        // generate a random integer to select the "correct" answer for this list of people
-        correctAnswerIndex = ThreadLocalRandom.current().nextInt(0, personList.size());
-        return new ShuffledList(personList, correctAnswerIndex);
-    }
-
-    public ShuffledList chooseCoworkers() {
+    synchronized public ShuffledList chooseCoworkers() {
         List<Person> listToQuery = new ArrayList<>();
         // shuffle the entire list of people
         Collections.shuffle(personList);
@@ -56,7 +34,7 @@ public class PeopleShuffler {
             listToQuery.add(personList.get(i));
         }
         // pseudo-randomly generate the index of the correct answer for this round
-        correctAnswerIndex = ThreadLocalRandom.current().nextInt(0, personList.size());
+        correctAnswerIndex = ThreadLocalRandom.current().nextInt(0, listToQuery.size());
         // create and return a ShuffledList from the randomly generated list of people
         Log.d(TAG, listToQuery.toString());
         return new ShuffledList(listToQuery, correctAnswerIndex);
@@ -67,9 +45,9 @@ public class PeopleShuffler {
         private List<Person> people;
         private int index;
 
-        public ShuffledList (List<Person> people, int personToQuery) {
+        public ShuffledList (List<Person> people, int index) {
             this.people = people;
-            this.index = personToQuery;
+            this.index = index;
         }
 
         public List<Person> getPeople () {
