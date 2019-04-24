bede9d0251cfc5c53110a54cfb8d1fd7f7c3ddf4
Nick Reeve <nick.reeve@willowtreeapps.com>
2019-04-24 11:15:48 -0400

    Person class: now implements Parcelable, so no crashing when the home button is pressed

--- app/src/main/java/android/example/nicksnamegame/game/Person.java
+++ app/src/main/java/android/example/nicksnamegame/game/Person.java
@@ -1,6 +1,9 @@
 package android.example.nicksnamegame.game;
 
-public class Person {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Person implements Parcelable {
     private String name;
     private String headshotUrl;
     private String id;
@@ -10,6 +13,24 @@ public class Person {
         this.headshotUrl = headshotUrl;
         this.id = id;
     }
+
+    protected Person(Parcel in) {
+        name = in.readString();
+        headshotUrl = in.readString();
+        id = in.readString();
+    }
+
+    public static final Creator<Person> CREATOR = new Creator<Person>() {
+        @Override
+        public Person createFromParcel(Parcel in) {
+            return new Person(in);
+        }
+
+        @Override
+        public Person[] newArray(int size) {
+            return new Person[size];
+        }
+    };
 
     public String getName() {
         return this.name;
@@ -23,4 +44,15 @@ public class Person {
         return this.id;
     }
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(headshotUrl);
+        dest.writeString(id);
+    }
 }
