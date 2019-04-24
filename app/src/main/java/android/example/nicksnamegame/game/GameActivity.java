1c693f4820d72adff2786580d290a9eccea78b78
Nick Reeve <nick.reeve@willowtreeapps.com>
2019-04-24 16:48:26 -0400

    added FAB for "next" button, + assoc. color, icon

--- app/src/main/java/android/example/nicksnamegame/game/GameActivity.java
+++ app/src/main/java/android/example/nicksnamegame/game/GameActivity.java
@@ -9,7 +9,11 @@ import android.example.nicksnamegame.R;
 import android.example.nicksnamegame.data.model.PersonConverter;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
@@ -20,11 +24,15 @@ public class GameActivity extends AppCompatActivity {
     private static final String SHUFFLED_LIST_KEY = "saved_shuffled_list";
 
     // TODO - add "next group" functionality
+
     /* TODO - tracking: if answered right on the first try, remove coworker from the pool.
-       If there are fewer than 6 names remaining, return "known" coworkers to pool
-       Once all coworkers are "known", prompt player to start over
+     *  If there aren't enough people left in the pool to fill the grid, allow "wrong" answers
+     *  to come from the list of known people, but make sure they're never the "right" answer again
+     *  until the pool is empty and the game is restarted
      */
 
+    private FloatingActionButton nextButton;
+    private ProgressBar progressBar;
     private RecyclerView people;
     private PhotoAdapter photoAdapter;
     private PeopleShuffler peopleShuffler;
@@ -36,45 +44,24 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        // prepare the nextButton FAB
+        nextButton = findViewById(R.id.next_button);
+
+        // show the loading indicator
+        progressBar = findViewById(R.id.progress_bar);
+        progressBar.setVisibility(View.VISIBLE);
+
+
+        // hide the recycler view while the game board loads
         people = (RecyclerView) findViewById(R.id.rv_photos);
+        people.setVisibility(View.INVISIBLE);
 
         int numberOfColumns = this.getResources().getInteger(R.integer.number_game_columns);
         GridLayoutManager photoManager = new GridLayoutManager(this, numberOfColumns);
         people.setLayoutManager(photoManager);
         people.setHasFixedSize(true);
-
-        final int numberOfPhotos = this.getResources().getInteger(R.integer.number_game_photos);
-
         if (savedInstanceState == null) {
-            new PersonConverter().retrievePersonList(new PersonConverter.PersonListHandler() {
-                @Override
-                public void onReceivePersonList(List<Person> personList) {
-                    // generate a new list of co-workers to play the game on
-                    if (personList == null) {
-                        Log.d(TAG, "List of co-workers not found");
-                    } else {
-                        // use retrieved list of people
-                        peopleShuffler = new PeopleShuffler(personList, numberOfPhotos);
-                        shuffledList = peopleShuffler.chooseCoworkers();
-                    }
-
-
-                    if (shuffledList == null) {
-                        Log.d(TAG, "Failed to load shuffled list");
-                    } else {
-                        photoAdapter = new PhotoAdapter(shuffledList, GameActivity.this);
-                        people.setAdapter(photoAdapter);
-                    }
-
-                    if (photoAdapter == null) {
-                        Log.d(TAG, "Failed to load photo adapter");
-                    } else {
-                        // set the text of the game prompt
-                        game_prompt_text_view = findViewById(R.id.game_prompt);
-                        game_prompt_text_view.setText(setName());
-                    }
-                }
-            });
+            generateGameGrid();
         } else {
             Log.d(TAG, "Retrieving state: " + savedInstanceState);
             shuffledList = savedInstanceState.getParcelable(SHUFFLED_LIST_KEY);
@@ -82,7 +69,10 @@ public class GameActivity extends AppCompatActivity {
             people.setAdapter(photoAdapter);
             game_prompt_text_view = findViewById(R.id.game_prompt);
             game_prompt_text_view.setText(setName());
+            game_prompt_text_view.setVisibility(View.VISIBLE);
         }
+        progressBar.setVisibility(View.INVISIBLE);
+        people.setVisibility(View.VISIBLE);
     }
 
     @Override
@@ -90,6 +80,41 @@ public class GameActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
         outState.putParcelable(PHOTO_ADAPTER_KEY, photoAdapter);
         outState.putParcelable(SHUFFLED_LIST_KEY, shuffledList);
+    }
+
+    protected void generateGameGrid() {
+
+        final int numberOfPhotos = this.getResources().getInteger(R.integer.number_game_photos);
+
+        new PersonConverter().retrievePersonList(new PersonConverter.PersonListHandler() {
+            @Override
+            public void onReceivePersonList(List<Person> personList) {
+                // generate a new list of co-workers to play the game on
+                if (personList == null) {
+                    Log.d(TAG, "List of co-workers not found");
+                } else {
+                    // use retrieved list of people
+                    peopleShuffler = new PeopleShuffler(personList, numberOfPhotos);
+                    shuffledList = peopleShuffler.chooseCoworkers();
+                }
+
+                if (shuffledList == null) {
+                    Log.d(TAG, "Failed to load shuffled list");
+                } else {
+                    photoAdapter = new PhotoAdapter(shuffledList, GameActivity.this);
+                    people.setAdapter(photoAdapter);
+                }
+
+                if (photoAdapter == null) {
+                    Log.d(TAG, "Failed to load photo adapter");
+                } else {
+                    // set the text of the game prompt
+                    game_prompt_text_view = findViewById(R.id.game_prompt);
+                    game_prompt_text_view.setText(setName());
+                    game_prompt_text_view.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 
     private String setName() {
