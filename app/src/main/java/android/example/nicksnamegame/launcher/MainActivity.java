98f5350fe07d1f7f722a251d0e2a0b91af7d3623
Nick Reeve <nick.reeve@willowtreeapps.com>
2019-04-29 12:08:33 -0400

    added menu w/ Settings option to launch screen

--- app/src/main/java/android/example/nicksnamegame/launcher/MainActivity.java
+++ app/src/main/java/android/example/nicksnamegame/launcher/MainActivity.java
@@ -6,9 +6,10 @@ import android.content.Intent;
 import android.example.nicksnamegame.R;
 import android.example.nicksnamegame.game.GameActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,12 +20,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         defaultGameButton = findViewById(R.id.default_game_button);
-        defaultGameButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent startGameIntent = new Intent(MainActivity.this, GameActivity.class);
-                startActivity(startGameIntent);
-            }
+        defaultGameButton.setOnClickListener(v -> {
+            Intent startGameIntent = new Intent(MainActivity.this, GameActivity.class);
+            startActivity(startGameIntent);
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.settings_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent startSettingsActivity = new Intent(this, SettingsActivity.class);
+            startActivity(startSettingsActivity);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
