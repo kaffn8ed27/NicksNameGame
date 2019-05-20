83c9f2e459d19a00cb095c60c1b5c94ab5e7b4cf
Nick Reeve <nick.reeve@willowtreeapps.com>
2019-05-20 14:36:47 -0400

    Separated tests to isolate failures
    All tests now have one assertion except for PeopleShuffler: must return
    all values sent in to it to ensure that the list sent in to the game
    board is populated as desired

--- app/src/test/java/android/example/nicksnamegame/data/model/HeadShotResponseTest.java
+++ app/src/test/java/android/example/nicksnamegame/data/model/HeadShotResponseTest.java
@@ -1,41 +1,45 @@
 package android.example.nicksnamegame.data.model;
 
-import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class HeadShotResponseTest {
 
     private HeadShotResponse testHeadShotResponseNoPrefix, testHeadShotResponseWithPrefix;
 
-    @Before
-    public void setUp() {
-        testHeadShotResponseNoPrefix = new HeadShotResponse(
-                "//head.shot",
-                "jpg",
-                50,
-                50);
+    @Test
+    public void testGetHeadShotUrlWithPrefix() {
 
         testHeadShotResponseWithPrefix = new HeadShotResponse(
                 "http://head.shot",
                 "jpg",
                 50,
                 50);
-    }
 
-    @Test
-    public void testGetHeadShotUrl() {
-        String testFormattedUrl = testHeadShotResponseNoPrefix.getHeadShotUrl();
         String testUntouchedUrl = testHeadShotResponseWithPrefix.getHeadShotUrl();
-
-        assertEquals(
-                "Head shot URL should be in proper URL format",
-                "https://head.shot",
-                testFormattedUrl);
         assertEquals(
                 "Properly formatted head shot URL should not have been altered",
                 "http://head.shot",
                 testUntouchedUrl);
+    }
+
+    @Test
+    public void testGetHeadShotUrlNoPrefix() {
+
+        testHeadShotResponseNoPrefix = new HeadShotResponse(
+                "//head.shot",
+                "jpg",
+                50,
+                50);
+
+        String testFormattedUrl = testHeadShotResponseNoPrefix.getHeadShotUrl();
+        assertEquals(
+                "Head shot URL should be in proper URL format",
+                "https://head.shot",
+                testFormattedUrl);
+
+
+
     }
 }
